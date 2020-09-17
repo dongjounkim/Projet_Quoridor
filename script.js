5edea9a2837ac16f46c82c01f33d1f1d3dd98528
dongjounkim <dongjounkim@gmail.com>
2020-09-17 15:36:22 +0200

    tjrs pas

---
+++ script.js
@@ -0,0 +1,16 @@
+var rows = 9;
+var columns = 9;
+
+
+
+$("#game-board").html("Hello");
+
+
+$(document).ready(function () {
+    for (var i = 0; i < columns; i++) {
+        for (var i = 0; i < rows; i++) {
+            $("#game-board").html("Hello");
+        }
+    }
+    $("#game-board").html("Hello");
+});
