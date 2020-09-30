c0fc0f03e834fbbf2d97620c4fba7fde66bf5923
robin-rvr <62430348+robin-rvr@users.noreply.github.com>
2020-09-30 11:57:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function dessinecase(i) {
+  $("#case").append(
+        `<div id="case${i}"
+              class="case"
+              style="left:80px;
+                     top:80px" >
+           </div>`)          
+}
+
+for (let i=0; i<17; i++) {
+    dessinecase(i)
+}
+
 var rows = 9;
 var columns = 9;
 
