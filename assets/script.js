5e9b0b9d8a3886f9fbb6c102d7a87884cb106257
dongjounkim <dongjounkim@gmail.com>
2020-10-23 17:13:26 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	function creeunrectangle(classe) {
 		let element = document.createElement('div');
 		element.classList.add(classe);
-		element.setAttribute("data-col",i);
-		element.setAttribute("data-lig",j);
+		element.setAttribute("data-col", i);
+		element.setAttribute("data-lig", j);
 		document.querySelector("#quadrillage").appendChild(element);
 	}
 
@@ -19,16 +19,38 @@ $(document).ready(function() {
 	}
 
 	$(".carre[data-col= 4][data-lig= 8]").append("<div class='pion1'></div>");
-
-	$(".carre").on('click', function() {
-		$(".pion1").remove();
-		$(this).append("<div class='pion1'></div>");
-	 });
-
-	/*
 	$(".carre[data-col= 4][data-lig= 0]").append("<div class='pion2'></div>");
-	$(".carre").on('click', function() {
-	  $(".pion2").remove();
-		$(this).append("<div class='pion2'></div>");
-	});*/
+
+	// $(".carre").on('click', function () {
+	// 	$(".pion1").remove();
+	// 	$(this).append("<div class='pion1'></div>");
+	// });
+
+	var player_number = 1;
+
+	$('#quadrillage').on('click', '.carre', function (event) {
+		// Swap current player
+		if (player_number === 1) {
+			$(".carre").on('click', function () {
+				$(".pion1").remove();
+				$(this).append("<div class='pion1'></div>");
+			});
+			player_number = 0;
+			console.log(player_number, "First");
+		} else {
+			$(".carre").on('click', function () {
+				$(".pion2").remove();
+				$(this).append("<div class='pion2'></div>");
+			});
+			player_number = 1;
+			console.log(player_number, 'second');
+		}
+	});
+
+	
+	// $(".carre[data-col= 4][data-lig= 0]").append("<div class='pion2'></div>");
+	// $(".carre").on('click', function() {
+	//   $(".pion2").remove();
+	// 	$(this).append("<div class='pion2'></div>");
+	// });
 });
